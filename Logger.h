8a74af1294acfed33a6b8a9e02b6dc9dbc357903
mikehan14 <hjljiayou1114@163.com>
2024-10-10 12:35:28 +0800

    Updated Logger.h

---
+++ Logger.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// 定义日志的级别 INFO 
+enum LogLevel:
+{
+    INFO, //普通信息
+    ERROR, //错误信息
+    FATAL, // core信息
+    DEBUG, //调试信息
+};
+
+//输出一个日志类
+class Logger
+{
+public:
+private:
+};
